7ebf1dec3d027c4307e72c81ce35a7bf0e93364a
Emmanuel Ezeani <monaoffice20@gmail.com>
2023-02-09 10:19:57 +0100

    added vendor autoprefixing for cross browser support😎

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 
-function buildStyles(){
+
+function build(){
     return src('core/**/*.scss')
     .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
     .pipe(dest('css'))
 }
 
 async function  watcher(){
-    watch(['core/**/*.scss'], buildStyles)
+    watch(['core/**/*.scss'], build)
     // console.log('Watcher is actively listening for changes in your css files')
 }
 
 
-exports.default = series(buildStyles, watcher)
+exports.default = series(build, watcher)
