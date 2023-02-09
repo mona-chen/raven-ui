01b0c3c1a8840d3ec9210c3f2ca9e4d98fb903ec
Emmanuel Ezeani <monaoffice20@gmail.com>
2023-02-09 18:49:31 +0100

    implemented grid-system, utility classes, and more 🎉

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,18 @@ const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
-
+// const purgecss = require('gulp-purgecss')
 
 function build(){
-    return src('core/**/*.scss')
+    return src('extend/**/*.scss')
     .pipe(sass())
+    // .pipe(purgecss({content: ['*.html']}))
     .pipe(postcss([autoprefixer()]))
     .pipe(dest('css'))
 }
 
 async function  watcher(){
-    watch(['core/**/*.scss'], build)
+    watch(['extend/**/*.scss', '*.html', 'core/**/*.scss'], build)
     // console.log('Watcher is actively listening for changes in your css files')
 }
 
