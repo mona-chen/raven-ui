2d00c019ddda049ca1cf09d7994dc2b4df2760db
Macbook <macbook@AKOT.local>
2023-02-13 15:52:22 +0100

    new style

--- gulpfile.js
+++ gulpfile.js
@@ -19,21 +19,3 @@ async function  watcher(){
 
 
 exports.default = series(build, watcher)
-
-// const gulp = require('gulp');
-// const inlinesource = require('gulp-inline-source');
-// const replace = require('gulp-replace');
-
-// gulp.task('default', () => {
-//   return gulp
-//     .src('./build/*.html')
-//     .pipe(replace('.js"></script>', '.js" inline></script>'))
-//     .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
-//     .pipe(
-//       inlinesource({
-//         compress: false,
-//         ignore: ['png'],
-//       })
-//     )
-//     .pipe(gulp.dest('./build'));
-// });
